ad4ea68ef8934df66be931a2fd7efa0d98f599b9
Martin Braun <martin.braun@kit.edu>
2012-10-23 11:46:20 +0200

    makexml: Fixed pretty printing for with and without lxml

--- src/grc_xml_generator.py
+++ src/grc_xml_generator.py
@@ -1,32 +1,15 @@
 import xml.etree.ElementTree as ET
+from util_functions import is_number, xml_indent
+
+### GRC XML Generator ########################################################
 try:
     import lxml.etree
     LXML_IMPORTED = True
 except ImportError:
     LXML_IMPORTED = False
 
-#from xml.etree import ElementTree
-#def indent(elem, level=0):
-    #i = "\n" + level*"  "
-    #if len(elem):
-        #if not elem.text or not elem.text.strip():
-            #elem.text = i + "  "
-        #if not elem.tail or not elem.tail.strip():
-            #elem.tail = i
-        #for elem in elem:
-            #indent(elem, level+1)
-        #if not elem.tail or not elem.tail.strip():
-            #elem.tail = i
-    #else:
-        #if level and (not elem.tail or not elem.tail.strip()):
-            #elem.tail = i
-
-#root = ElementTree.parse('/tmp/xmlfile').getroot()
-#indent(root)
-
-from util_functions import is_number
-
 class GRCXMLGenerator(object):
+    """ Create and write the XML bindings for a GRC block. """
     def __init__(self, modname=None, blockname=None, doc=None, params=None, iosig=None):
         """docstring for __init__"""
         params_list = ['$'+s['key'] for s in params if s['in_constructor']]
@@ -40,8 +23,25 @@ class GRCXMLGenerator(object):
         self.iosig = iosig
         self.doc = doc
         self.root = None
+        if LXML_IMPORTED:
+            self._prettyprint = self._lxml_prettyprint
+        else:
+            self._prettyprint = self._manual_prettyprint
+
+    def _lxml_prettyprint(self):
+        """ XML pretty printer using lxml """
+        return lxml.etree.tostring(
+                   lxml.etree.fromstring(ET.tostring(self.root, encoding="UTF-8")),
+                   pretty_print=True
+               )
+
+    def _manual_prettyprint(self):
+        """ XML pretty printer using xml_indent """
+        xml_indent(self.root)
+        return ET.tostring(self.root, encoding="UTF-8")
 
     def make_xml(self):
+        """ Create the actual tag tree """
         root = ET.Element("block")
         iosig = self.iosig
         for tag in self._header.keys():
@@ -53,7 +53,7 @@ class GRCXMLGenerator(object):
             ET.SubElement(param_tag, 'key').text = param['key']
             ET.SubElement(param_tag, 'type').text = param['type']
             ET.SubElement(param_tag, 'value').text = param['default']
-        for inout in iosig.keys():
+        for inout in sorted(iosig.keys()):
             for i in range(len(iosig[inout]['type'])):
                 s_tag = ET.SubElement(root, {'in': 'sink', 'out': 'source'}[inout])
                 ET.SubElement(s_tag, 'name').text = inout
@@ -72,29 +72,10 @@ class GRCXMLGenerator(object):
                                                                   len(iosig[inout]['type'])+1)
         if self.doc is not None:
             ET.SubElement(root, 'doc').text = self.doc
-        #tree = ET.ElementTree(root)
         self.root = root
 
     def save(self, filename):
-        """docstring for save"""
+        """ Write the XML file """
         self.make_xml()
-        #print ET.tostring(self.root, encoding="UTF-8")
-        #self.tree.write(filename, encoding="UTF-8", xml_declaration=True)
-        open(filename, 'w').write(
-                lxml.etree.tostring(
-                    lxml.etree.fromstring(ET.tostring(self.root, encoding="UTF-8")),
-                    pretty_print=True
-                )
-        )
-
-
-
-if __name__ == "__main__":
-    data = {'name': 'FooBar',
-            'key': 'howto_foo_ff',
-            'category': 'HOWTO',
-            'import': 'import howto',
-            'make': 'howto.foo_ff'}
-    gen = GRCXMLGenerator(data)
-    gen.make_xml()
+        open(filename, 'w').write(self._prettyprint())
 
