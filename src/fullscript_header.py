2e433a9770975077163b25f24efb8bfeefd551b6
Martin Braun <martin.braun@kit.edu>
2012-11-02 10:17:33 +0100

    add: Bugfix, missing Cheetah import

--- src/fullscript_header.py
+++ src/fullscript_header.py
@@ -26,6 +26,6 @@ import base64
 import tarfile
 from datetime import datetime
 from optparse import OptionParser, OptionGroup
-from string import Template
+import Cheetah.Template
 import xml.etree.ElementTree as ET
 
