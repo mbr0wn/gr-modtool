c1b2d5ec44e0f84ef69e614ca7153ec7ba3e63d1
Martin Braun <martin.braun@kit.edu>
2012-11-06 10:48:29 +0100

    Removed redundant functions from code_generator.py

--- gr_modtool.py
+++ gr_modtool.py
@@ -459,7 +459,7 @@ gr_modtool.py help <command> -- Shows the help for a given command. '''
 ### Code generator class #####################################################
 class GRMTemplate(Cheetah.Template.Template):
     """ An extended template class """
-    def __init__(self, src, searchList=[]):
+    def __init__(self, src, searchList):
         self.grtypelist = {
                 'sync': 'gr_sync_block',
                 'decimator': 'gr_sync_decimator',
@@ -468,27 +468,12 @@ class GRMTemplate(Cheetah.Template.Template):
                 'hiercpp': 'gr_hier_block2',
                 'noblock': '',
                 'hierpython': ''}
+        searchList['str_to_fancyc_comment'] = str_to_fancyc_comment
+        searchList['str_to_python_comment'] = str_to_python_comment
+        searchList['strip_default_values'] = strip_default_values
+        searchList['strip_arg_types'] = strip_arg_types
         Cheetah.Template.Template.__init__(self, src, searchList=searchList)
         self.grblocktype = self.grtypelist[searchList['blocktype']]
-    def strip_default_values(string):
-        """ Strip default values from a C++ argument list. """
-        return re.compile(" *=[^,)]*").sub("", string)
-    def strip_arg_types(string):
-        """" Strip the argument types from a list of arguments
-        Example: "int arg1, double arg2" -> "arg1, arg2" """
-        string = re.compile(" *=[^,)]*").sub("", string) # FIXME this should call strip_arg_types
-        return ", ".join([part.strip().split(' ')[-1] for part in string.split(',')])
-    def str_to_fancyc_comment(text):
-        """ Return a string as a C formatted comment. """
-        l_lines = text.splitlines()
-        outstr = "/* " + l_lines[0] + "\n"
-        for line in l_lines[1:]:
-            outstr += " * " + line + "\n"
-        outstr += " */\n"
-        return outstr
-    def str_to_python_comment(text):
-        """ Return a string as a Python formatted comment. """
-        return re.compile('^', re.MULTILINE).sub('# ', text)
 
 def get_template(tpl_id, **kwargs):
     """ Return the template given by tpl_id, parsed through Cheetah """
@@ -617,14 +602,15 @@ class ModTool(object):
                 help="Don't do anything in the swig/ subdirectory.")
         ogroup.add_option("--skip-python", action="store_true", default=False,
                 help="Don't do anything in the python/ subdirectory.")
-        ogroup.add_option("--skip-grc", action="store_true", default=True,
+        ogroup.add_option("--skip-grc", action="store_true", default=False,
                 help="Don't do anything in the grc/ subdirectory.")
         parser.add_option_group(ogroup)
         return parser
 
     def _setup_files(self):
         """ Initialise the self._file[] dictionary """
-        self._file['swig'] = os.path.join('swig', self._get_mainswigfile())
+        if not self._skip_subdirs['swig']:
+            self._file['swig'] = os.path.join('swig', self._get_mainswigfile())
         self._file['qalib'] = os.path.join('lib', 'qa_%s.cc' % self._info['modname'])
         self._file['pyinit'] = os.path.join('python', '__init__.py')
         self._file['cmlib'] = os.path.join('lib', 'CMakeLists.txt')
@@ -642,22 +628,23 @@ class ModTool(object):
             sys.exit(1)
         print "Operating in directory " + self._dir
 
-        if options.skip_lib:
-            print "Force-skipping 'lib'."
-            self._skip_subdirs['lib'] = True
-        if options.skip_python:
-            print "Force-skipping 'python'."
-            self._skip_subdirs['python'] = True
-        if options.skip_swig:
-            print "Force-skipping 'swig'."
-            self._skip_subdirs['swig'] = True
-
         if options.module_name is not None:
             self._info['modname'] = options.module_name
         else:
             self._info['modname'] = get_modname()
         print "GNU Radio module name identified: " + self._info['modname']
+
+        if options.skip_lib:
+            self._skip_subdirs['lib'] = True
+        if options.skip_python:
+            self._skip_subdirs['python'] = True
+        if options.skip_swig or self._get_mainswigfile() is None:
+            self._skip_subdirs['swig'] = True
+        if options.skip_grc:
+            self._skip_subdirs['grc'] = True
+
         self._info['blockname'] = options.block_name
+        self._info['includedir'] = 'include'
         self.options = options
         self._setup_files()
 
@@ -669,7 +656,7 @@ class ModTool(object):
 
     def _check_directory(self, directory):
         """ Guesses if dir is a valid GNU Radio module directory by looking for
-        gnuradio.project and at least one of the subdirs lib/, python/ and swig/.
+        CMakeLists.txt and at least one of the subdirs lib/, python/ and swig/.
         Changes the directory, if valid. """
         has_makefile = False
         try:
--- src/code_generator.py
+++ src/code_generator.py
@@ -3,11 +3,15 @@
 import re
 from templates import Templates
 import Cheetah.Template
+from util_functions import str_to_fancyc_comment
+from util_functions import str_to_python_comment
+from util_functions import strip_default_values
+from util_functions import strip_arg_types
 
 ### Code generator class #####################################################
 class GRMTemplate(Cheetah.Template.Template):
     """ An extended template class """
-    def __init__(self, src, searchList=[]):
+    def __init__(self, src, searchList):
         self.grtypelist = {
                 'sync': 'gr_sync_block',
                 'decimator': 'gr_sync_decimator',
@@ -16,27 +20,12 @@ class GRMTemplate(Cheetah.Template.Template):
                 'hiercpp': 'gr_hier_block2',
                 'noblock': '',
                 'hierpython': ''}
+        searchList['str_to_fancyc_comment'] = str_to_fancyc_comment
+        searchList['str_to_python_comment'] = str_to_python_comment
+        searchList['strip_default_values'] = strip_default_values
+        searchList['strip_arg_types'] = strip_arg_types
         Cheetah.Template.Template.__init__(self, src, searchList=searchList)
         self.grblocktype = self.grtypelist[searchList['blocktype']]
-    def strip_default_values(string):
-        """ Strip default values from a C++ argument list. """
-        return re.compile(" *=[^,)]*").sub("", string)
-    def strip_arg_types(string):
-        """" Strip the argument types from a list of arguments
-        Example: "int arg1, double arg2" -> "arg1, arg2" """
-        string = re.compile(" *=[^,)]*").sub("", string) # FIXME this should call strip_arg_types
-        return ", ".join([part.strip().split(' ')[-1] for part in string.split(',')])
-    def str_to_fancyc_comment(text):
-        """ Return a string as a C formatted comment. """
-        l_lines = text.splitlines()
-        outstr = "/* " + l_lines[0] + "\n"
-        for line in l_lines[1:]:
-            outstr += " * " + line + "\n"
-        outstr += " */\n"
-        return outstr
-    def str_to_python_comment(text):
-        """ Return a string as a Python formatted comment. """
-        return re.compile('^', re.MULTILINE).sub('# ', text)
 
 def get_template(tpl_id, **kwargs):
     """ Return the template given by tpl_id, parsed through Cheetah """
